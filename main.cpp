3a237e037f7560ec52bfe6c8b24688de9f13756a
ChernovVA <vasily@tchernov.com>
2021-05-17 11:03:45 +0300

    code: начало работы

---
+++ main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<double> input_numbers(size_t count)
+{
+    vector<double> input(count);
+    for (size_t i = 0; i < count; i++)
+    {
+        cerr << "Number " << i+1 << ") ";
+        cin >> input[i];
+    }
+    return input;
+}
+
+void find_minmax(vector<double> numbers, double& min, double& max)
+{
+    min = numbers[0];
+    max = numbers[0];
+
+    for(double elem: numbers)
+    {
+        if(elem > max)
+        {
+            max = elem;
+        }
+        else if(elem < min)
+        {
+            min = elem;
+        }
+    }
+}
+
+int main()
+{
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_HIGH = SCREEN_WIDTH - 3 - 1;
+    size_t number_count, bin_count, max_in_bin=0;
+    int i;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+    const auto numbers = input_numbers(number_count);
+
+    cerr << "Enter bin count: ";
+    cin >> bin_count;
+    vector <size_t> bins (bin_count);
+
+    double min, max;
+
+    find_minmax(vector<double> numbers, min, max);
+
+    double bin_size = (max - min) / bin_count;
+
+
+    for (size_t i = 0; i < number_count; i++)
+    {
+        bool flag = false;
+        for (size_t j = 0; (j < bin_count - 1) && !flag; j++)
+        {
+            auto lo = min + j * bin_size;
+            auto hi = min + (j + 1) * bin_size;
+
+            if ((lo <= numbers[i]) && (numbers[i] < hi))
+            {
+                bins[j]++;
+                flag = true;
+            }
+            if (bins[j]>max_in_bin)
+            {
+                max_in_bin=bins[j];
+            }
+        }
+        if (!flag)
+        {
+            bins[bin_count - 1]++;
+        }
+        if (bins[bin_count - 1]>max_in_bin)
+        {
+            max_in_bin=bins[bin_count - 1];
+        }
+    }
+
+    if (max_in_bin > MAX_HIGH)
+    {
+        for(size_t size: bins)
+        {
+            if(size < 10)
+            {
+                cout << "  ";
+            }
+            else
+            if(size < 100)
+            {
+                cout << " ";
+            }
+            cout << size << "|";
+            size_t height = MAX_HIGH * (static_cast<double>(size) / max_in_bin);
+            while(height != 0)
+            {
+                cout << "*";
+                height=height-1;
+            }
+            cout << endl;
+        }
+    }
+    else
+    {
+        for(size_t size: bins)
+        {
+            if(size < 10)
+            {
+                cout << "  ";
+            }
+            else
+            if(size < 100)
+            {
+                cout << " ";
+            }
+            cout << size << "|";
+            while(size != 0)
+            {
+                cout << "*";
+                size=size-1;
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
