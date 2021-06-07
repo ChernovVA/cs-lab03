256ab8d38f5b731e0231f520b0d5e8f93efd862c
u211-09 <u211-09@public.mpei.local>
2021-06-07 12:43:34 +0300

    WIP: добавлено решение варианта

---
+++ lab03_test_1/test_1.cpp
@@ -0,0 +1,18 @@
+#include "histogram.h"
+
+#include <cassert>
+#include <fstream>
+
+void test_width+()
+{
+    stringstream in("400")
+    width_input ({1, 2, 3}, std::in)
+}
+
+int main()
+{
+    test_width+();
+    test_width<70();
+    return 0;
+}
+
