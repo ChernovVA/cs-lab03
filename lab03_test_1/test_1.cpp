1a4dc90a196916c32903effccdecb6231cce4c9e
ChernovVA <vasily@tchernov.com>
2021-10-08 12:00:37 +0300

    code: добавлено решение варианта

--- lab03_test_1/test_1.cpp
+++ lab03_test_1/test_1.cpp
@@ -1,18 +1,35 @@
 #include "histogram.h"
 
 #include <cassert>
-#include <fstream>
+#include <sstream>
 
-void test_width+()
+void test1 ()
 {
-    stringstream in("400")
-    width_input ({1, 2, 3}, std::in)
+    stringstream in_t ("400");
+    double res = input_width (5, 10, in_t);
+    assert(res == 400);
+}
+void test2 ()
+{
+    stringstream in_t ("1000");
+    double res = input_width (5, 10, in_t);
+    stringstream im ("500");
+    res = input_width (5, 10, in_t);
+    assert(res == 500);
+
+}
+void test3 ()
+{
+    stringstream in_t ("30");
+    double res = input_width (5, 10, in_t);
+    assert(res == 30);
 }
 
 int main()
 {
-    test_width+();
-    test_width<70();
+    test1();
+    test2();
+    test3();
     return 0;
 }
 
